fdcc43d5f5e524d3c852ffd9947ea367e7a42a09
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-12-13 19:28:10 +0900

    feat:P21

--- src/test/scala/P21Spec.scala
+++ src/test/scala/P21Spec.scala
@@ -7,10 +7,6 @@ class P21Spec extends AnyFunSpec with Diagrams {
     it("return list added 6") {
       assert(P21.insertAt(6, 0, List(8, 4, 1, 5)) == List(6, 8, 4, 1, 5))
       assert(P21.insertAt(6, 2, List(8, 4, 1, 5)) == List(8, 4, 6, 1, 5))
-      // val result: (List[Int], Int) = P20.removeAt(2, List(1, 2, 3, 6))
-//
-      // assert(result._1 == (List(1, 2, 6)))
-      // assert(result._2 == 3)
     }
   }
 }
